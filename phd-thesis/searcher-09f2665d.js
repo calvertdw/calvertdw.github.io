81bf24c35d61b128b13fd3e9828669a975e09e02
Duncan Calvert <dcalvert@ihmc.org>
2026-05-25 21:51:38 -0500

    Add introduction, metrics, and story.

--- phd-thesis/searcher-09f2665d.js
+++ phd-thesis/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-62645ed1.js',
+                path_to_root + 'searchindex-14326c61.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
