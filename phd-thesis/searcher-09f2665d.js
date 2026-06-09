814e634f6b7fef543c22be88fdb7e1417b5259cf
Duncan Calvert <dcalvert@ihmc.org>
2026-06-09 13:03:15 -0500

    Fix table and update PDF draft

--- phd-thesis/searcher-09f2665d.js
+++ phd-thesis/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d3ba1492.js',
+                path_to_root + 'searchindex-cc4d89e8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
